4560484865d334eb62e5f525848f213e2dd4cc55
Rodrigo Juarez Velasco <20161224@itoaxaca.edu.mx>
2026-02-04 17:06:57 -0600

    Fix Laravel MySQL SSL options for Aiven

--- config/database.php
+++ config/database.php
@@ -58,10 +58,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-          'options' => extension_loaded('pdo_mysql') ? array_filter([
+         'options' => extension_loaded('pdo_mysql') ? [
     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
-]) : [],
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+] : [],
+
 
         ],
 
